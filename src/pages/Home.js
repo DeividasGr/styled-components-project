741e55af978b6f553500f6ed67847c9ceb3df0b4
Gruzinas1 <dgruzdzevicius@gmail.com>
2021-03-30 15:17:15 +0300

    rooms component setup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import About from '../components/home/About';
 import Header from '../components/home/Header';
+import Rooms from '../components/home/Room';
 
 function Home() {
   return (
     <>
       <Header />
       <About />
+      <Rooms />
     </>
   );
 }
